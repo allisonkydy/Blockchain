549e8773d43547389587741086430176fcecc186
allisonkydy <allisnelly@gmail.com>
2020-02-13 16:00:53 -0600

    add CORS to server, add list of transactions to wallet

--- basic_wallet_p/wallet/src/App.js
+++ basic_wallet_p/wallet/src/App.js
@@ -1,23 +1,59 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 import IdForm from './components/IdForm';
 import { useLocalStorage } from './hooks/useLocalStorage';
 
+const serverURI = 'http://localhost:5000'
+
 function App() {
-  const [id, setId] = useLocalStorage('id', null)
-  const [editingId, setEditingId] = useState(false);
+  const [id, setId] = useLocalStorage('id', null);
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(() => {
+    if (id !== null) {
+      axios.get(serverURI + '/chain')
+        .then(res => {
+          const allTransactions = res.data.chain.map(block => block.transactions).flat();
+          const myTransactions = allTransactions.filter(t => {
+            return t.recipient === id || t.sender === id;
+          })
+          // console.log(myTransactions)
+          setTransactions(myTransactions);
+        })
+        .catch(err => console.log(err))
+
+    }
+  }, [id])
 
   const handleId = (new_id) => {
     setId(new_id);
-    setEditingId(false);
   }
 
-  return id === null || editingId === true ? (
+  return id === null ? (
     <IdForm handleId={handleId} />
   ) : (
     <div className="App">
-      what's up {id}
-      <button onClick={() => setEditingId(true)}>edit id</button>
+      <h1>
+        what's up {id}
+      </h1>
+      <button onClick={() => setId(null)}>edit id</button>
+      <h2>Past transactions: </h2>
+      {transactions.map(t => {
+        return (
+          <ul>
+            <li>
+              Sender: {t.sender}
+            </li>
+            <li>
+              Recipient: {t.recipient}
+            </li>
+            <li>
+              Amount: {t.amount}
+            </li>
+          </ul>
+        )
+      })}
     </div>
   );
 }
